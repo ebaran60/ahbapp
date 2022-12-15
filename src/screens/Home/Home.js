877f96e87450192fff8213bfa2085b6653c6fa33
emregurbuz <emre_gurbuz@yahoo.com>
2022-12-16 00:46:51 +0300

    revize

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,5 +1,5 @@
 import { View, SafeAreaView, FlatList } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './Home.styles';
 import Header from '../../Components/Header';
@@ -7,9 +7,17 @@ import Search from '../../Components/Searchbar';
 import MainEvent from '../../Components/mainEvent';
 import Titles from '../../Components/Titles';
 import Events from '../../Components/Events';
-
+import { width } from '../../config';
 const Home = () => {
-
+  const [selectedSlider, setSelectedSlider] = useState(0);
+  const scrolling = (scrollLocation) => {
+    if (scrollLocation == 0) {
+      setSelectedSlider(0)
+    }
+    else {
+      setSelectedSlider(Math.ceil(scrollLocation / width * 0.9))
+    }
+  }
   const Data = [
     {
       id: 1,
@@ -34,14 +42,20 @@ const Home = () => {
         <Search placeholder="Search" iconName="search" />
         <MainEvent />
         <Titles />
-        <FlatList 
-        horizontal
-        pagingEnabled
-        showsHorizontalScrollIndicator={false}
-        data={Data} 
-        renderItem={({ item }) =>
-          <Events item={item} />
-        } />
+        <FlatList
+          onScroll={event => scrolling(event.nativeEvent.contentOffset.x)}
+          horizontal
+          pagingEnabled
+          showsHorizontalScrollIndicator={false}
+          data={Data}
+          renderItem={({ item, index }) =>
+            <Events item={item} />
+          } />
+        <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf: 'center', marginTop: 5 }}>
+          {Data.map((item, index) =>
+            <View key={index} style={{ width: selectedSlider == index?16:8, height: 8, borderRadius: 8, marginHorizontal: 2, backgroundColor: selectedSlider == index ? 'red' : 'black' }} />
+          )}
+        </View>
       </View>
 
     </SafeAreaView>

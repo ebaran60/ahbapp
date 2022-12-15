bf60920bdd92937667bbc20cc03d25e8b8b1b6c5
emregurbuz <emre_gurbuz@yahoo.com>
2022-12-16 00:31:18 +0300

    düzenleme

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -2,20 +2,22 @@ import { View, Text, SafeAreaView, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 import styles from './Header.styles';
 
-const Header = ({userName}) => {
+const Header = ({ userName }) => {
   return (
-    <SafeAreaView style={styles.container} >
-      <Image style={styles.image} source={require('../../assets/images/profile.png')} />
-      <View style={styles.body_container}>
-        <Text style={styles.merhaba}>Merhaba</Text>
-        <Text style={styles.name}>{userName}</Text>
+    <View style={styles.container} >
+      <View style={styles.leftContainer}>
+        <Image style={styles.image} source={require('../../assets/images/profile.png')} />
+        <View style={styles.body_container}>
+          <Text style={styles.merhaba}>Merhaba</Text>
+          <Text style={styles.name}>{userName}</Text>
+        </View>
       </View>
       <TouchableOpacity style={styles.touchable_container}>
-        <View style={{height:4, width:20,backgroundColor:'black',borderRadius:5,}}/>
-        <View style={{height:4, width:15,backgroundColor:'black',marginTop:2,marginBottom:2,borderRadius:5}}/>
-        <View style={{height:4, width:10,backgroundColor:'black',borderRadius:5,}}/>
+        <View style={{ height: 4, width: 20, backgroundColor: 'black', borderRadius: 5, }} />
+        <View style={{ height: 4, width: 15, backgroundColor: 'black', marginTop: 2, marginBottom: 2, borderRadius: 5 }} />
+        <View style={{ height: 4, width: 10, backgroundColor: 'black', borderRadius: 5, }} />
       </TouchableOpacity>
-    </SafeAreaView>
+    </View>
   )
 }
 
--- src/Components/Header/Header.styles.js
+++ src/Components/Header/Header.styles.js
@@ -1,26 +1,34 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container:{
-        flexDirection:'row',
-        margin:5,
-        backgroundColor:'blue'
+    container: {
+        flexDirection: 'row',
+        margin: 5,
+        backgroundColor: 'rgba(0,0,0,0.3)',
+        justifyContent: 'space-between',
+        padding:10,
+        borderRadius:5,
+        alignItems:'center'
     },
-    image:{
-        width:40,
-        height:40,
-        marginRight:5,
-        
+    image: {
+        width: 40,
+        height: 40,
+        marginRight: 5,
+
     },
-    body_container:{
-        flex: 1,
+    leftContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
     },
-    merhaba:{
-        fontSize:14,
-        tintColor:'#B9B9B9',
+    body_container: {
+        marginLeft: '2%'
     },
-    name:{},
-    touchable_container:{
-        textAlign: 'left',
+    merhaba: {
+        fontSize: 14,
+        tintColor: '#B9B9B9',
+    },
+    name: {},
+    touchable_container: {
+        alignItems:'flex-end'
     },
 });

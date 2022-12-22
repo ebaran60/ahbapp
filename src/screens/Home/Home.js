a7bac869b5da3bba6f7b87c3d4bc394ff47583a5
Emre Gürbüz <emregurbuz@Emres-MacBook-Air.local>
2022-12-23 01:46:31 +0300

    düzenleme

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -1,4 +1,4 @@
-import { View, SafeAreaView,Text, FlatList, ScrollView } from 'react-native'
+import { View, SafeAreaView, Text, FlatList, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import { Data, Titless, Activity } from '../../mock';
 import Proffer from '../../Components/Proffer';
@@ -23,12 +23,12 @@ const Home = ({ navigation }) => {
     }
   }
   const aramaYap = (keyword) => {
-    if(keyword.length>0){
+    if (keyword.length > 0) {
       setDataState(Data.filter(item => item.id == keyword))
     }
-    else{
+    else {
       setDataState(Data)
-    }1
+    } 1
   }
   const handleProductSelect = (source) => {
     navigation.navigate('Detail', { source })
@@ -42,51 +42,51 @@ const Home = ({ navigation }) => {
       <ScrollView>
         <Header userName="Alexandre S." />
         <Search value={searchKeyword} onChangeText={text => aramaYap(text)} />
-        {dataState.length==0?
-        <Text>Noresult</Text>:
-        <View>
-          <FlatList
-            onScroll={event => scrolling(event.nativeEvent.contentOffset.x)}
-            horizontal
-            pagingEnabled
-            showsHorizontalScrollIndicator={false}
-            data={dataState}
-            renderItem={({ item, index }) =>
-              <MainEvent item={item} onSelect={() => handleProductSelect(item.source)} />
-            } />
-          <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf: 'center', marginTop: 5 }}>
-            {dataState.map((item, index) =>
-              <View key={index} style={{ width: selectedSlider == index ? 29 : 14, height: 5, borderRadius: 8, marginHorizontal: 2, backgroundColor: selectedSlider == index ? colors.gray : '#D9D9D9' }} />
-            )}
+        {dataState.length == 0
+          ? <Text>Noresult</Text>
+          : <View>
+            <FlatList
+              onScroll={event => scrolling(event.nativeEvent.contentOffset.x)}
+              horizontal
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              data={dataState}
+              renderItem={({ item, index }) =>
+                <MainEvent item={item} onSelect={() => handleProductSelect(item.source)} />
+              } />
+            <View style={{ flexDirection: 'row', alignItems: 'center', alignSelf: 'center', marginTop: 5 }}>
+              {dataState.map((item, index) =>
+                <View key={index} style={{ width: selectedSlider == index ? 29 : 14, height: 5, borderRadius: 8, marginHorizontal: 2, backgroundColor: selectedSlider == index ? colors.gray : '#D9D9D9' }} />
+              )}
+            </View>
+            <Proffer title="Trending" />
+            <FlatList
+              horizontal
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              data={Titless}
+              renderItem={({ item }) =>
+                <Titles title={item.title} />
+              }
+            />
+            <FlatList
+              horizontal
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              data={Activity}
+              renderItem={({ item }) =>
+                <Events item={item} />
+              } />
+            <Proffer title="Nearby Your Location" />
+            <FlatList
+              horizontal
+              pagingEnabled
+              showsHorizontalScrollIndicator={false}
+              data={Activity}
+              renderItem={({ item }) =>
+                <Events item={item} />
+              } />
           </View>
-          <Proffer title="Trending" />
-          <FlatList
-            horizontal
-            pagingEnabled
-            showsHorizontalScrollIndicator={false}
-            data={Titless}
-            renderItem={({ item }) =>
-              <Titles title={item.title} />
-            }
-          />
-          <FlatList
-            horizontal
-            pagingEnabled
-            showsHorizontalScrollIndicator={false}
-            data={Activity}
-            renderItem={({ item }) =>
-              <Events item={item} />
-            } />
-          <Proffer title="Nearby Your Location" />
-          <FlatList
-            horizontal
-            pagingEnabled
-            showsHorizontalScrollIndicator={false}
-            data={Activity}
-            renderItem={({ item }) =>
-              <Events item={item} />
-            } />
-        </View>
         }
       </ScrollView>
     </SafeAreaView>

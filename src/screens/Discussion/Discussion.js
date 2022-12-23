81dca8e5b353011e09e688de554ecd71cad54faf
Emre Gürbüz <emregurbuz@Emres-MacBook-Air.local>
2022-12-23 22:30:55 +0300

    düzeltme

--- src/screens/Discussion/Discussion.js
+++ src/screens/Discussion/Discussion.js
@@ -31,7 +31,6 @@ const Discussion = () => {
   const sendMessage = (message) => {
     socket.emit("sendmessage", { userId, message, })
   }
-git 
   return (
     <SafeAreaView style={styles.container}>
       <FlatList
